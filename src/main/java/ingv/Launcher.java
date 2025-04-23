90149f8f5684928430ced285cae306647b55e997
Emanuele Tocci <emato03.felix@gmail.com>
2025-04-23 16:25:50 +0200

    add launcher class

---
+++ src/main/java/ingv/Launcher.java
@@ -0,0 +1,9 @@
+package ingv;
+
+// This class is only used my shade plugin to create a jar file
+public class Launcher {
+    public static void main(String[] args) {
+        INGVReport.main(args);
+    }
+}
+
